6513b6dbe2b5992bd8e94c7ff1fc6fc5e04a21f2
ajbrusic <ajbrusic@buffalo.edu>
2019-11-02 22:44:50 -0400

    set up list

--- gui/gui.py
+++ gui/gui.py
@@ -4,39 +4,57 @@ from curses.textpad import Textbox, rectangle
 
 def main(stdscr):
     h, w = stdscr.getmaxyx()
+
+    #used to help what is labeled
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+
     searching = False
-    search = "Type name here: "
-    maxlen = w - len(search)
+    albums = False
+
+    alb_list = ["hello",  "there", "general", "kenobi"]
+    alb_idx = 0
+
+    # song_list = ["general", "kenobi"]
+    # song_idx = 0
+
+
+
+    #used to help build the search bar
+    header = "Type album name here: "
+    search = ""
+    maxlen = w - len(header)
     search_len = 0
 
-    #set up info for albums
-    alb_index = 0
-    alb_height = 40
-    alb = curses.newpad(alb_height, w)
-
-    #set up info for songs
-    song_index = 0
-    song_height = 20
-    songs = curses.newpad(song_height, w)
 
     while True:
         key = stdscr.getch()
 
         stdscr.clear()
 
+        #track key navigation conmmands
         if key == curses.KEY_ENTER or key in [10, 13]:
             if searching == False:
                 searching = True
-            else:
-                searching = False
-                search = "Type name here: "
-                search_len = 0
 
+            elif searching and search != "":
+                searching = False
+                albums = True
+            elif albums:
+                albums = False
+
+        elif key == curses.KEY_UP and albums:
+            if alb_idx > 0:
+                alb_idx -= 1
+    
+        elif key == curses.KEY_DOWN and albums:
+            if alb_idx < len(alb_list) - 1:
+                alb_idx += 1
         elif key == curses.KEY_EXIT:
             break
 
         #search function
-        if searching == True:
+        if searching:
             if search_len < maxlen - 1:
                 if (key >= ord('A') and key <= ord('Z')) or (key >= ord('a') and key <= ord('z')):
                     search += chr(key)
@@ -44,17 +62,34 @@ def main(stdscr):
                 elif key == ord(' '):
                     search += ' '
                     search_len += 1
-
+            #deletes characters
             if key == curses.KEY_BACKSPACE:
                 if search_len > 0:
                     search = search[:-1]
                     search_len -= 1
-            
-                
-            
+            bar = header + search
+            stdscr.addstr(0,0,bar)
+        
+        #parse through albums
+        elif albums:
             stdscr.clear()
-            stdscr.addstr(0,0,search)
-            stdscr.refresh()
+            stdscr.addstr(search)
+            for i in range(0,w):
+                stdscr.addch(1,i,'*',curses.color_pair(2))
+            line = 2
+            for a in range(0,len(alb_list)):
+                if a >= h-2:
+                    break
+                elif a == alb_idx:
+                    stdscr.addstr(line,0,alb_list[a], curses.color_pair(1))
+                else:
+                    stdscr.addstr(line,0,alb_list[a])
+
+                line += 1
+
+        # elif appState == "albums"
+        # elif appState == "songs"
+
 
         stdscr.refresh
 
